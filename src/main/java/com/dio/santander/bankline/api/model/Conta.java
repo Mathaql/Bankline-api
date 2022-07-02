e04be26d2d8c37632ffbede53a4c3fccf5abb05b
Matheus Henrique <mhalquino@gmail.com>
2022-07-02 11:16:38 -0300

    feat: Mapeando as minhas entidades com JPA

--- src/main/java/com/dio/santander/bankline/api/model/Conta.java
+++ src/main/java/com/dio/santander/bankline/api/model/Conta.java
@@ -1,8 +1,13 @@
 package com.dio.santander.bankline.api.model;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
 public class Conta {
-		
-	private Long numero;
+		@Column(name="conta_numero")
+		private Long numero;
+		@Column(name="conta_saldo")
 		private Double saldo;
 		
 		public Long getNumero() {
--- src/main/java/com/dio/santander/bankline/api/model/Correntista.java
+++ src/main/java/com/dio/santander/bankline/api/model/Correntista.java
@@ -1,17 +1,31 @@
 package com.dio.santander.bankline.api.model;
 
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tab_correntista")
 public class Correntista {
-		
+		@Id
+		@GeneratedValue(strategy=GenerationType.IDENTITY)
 		private Integer id;
+		@Column(length=11)
 		private String cpf;
+		@Column(length=40)
 		private String nome;
 		
-		private String conta;
+		@Embedded
+		private Conta conta;
 		
-		public String getConta() {
+		public Conta getConta() {
 			return conta;
 		}
-		public void setConta(String conta) {
+		public void setConta(Conta conta) {
 			this.conta = conta;
 		}
 		
